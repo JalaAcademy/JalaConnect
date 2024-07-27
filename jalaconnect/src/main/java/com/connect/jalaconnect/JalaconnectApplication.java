00f4659554b023421f08754569e5257829398898
jacob-gotnoskills <nishantchaki123@gmail.com>
2024-07-27 14:02:39 +0530

    api v0

---
+++ jalaconnect/src/main/java/com/connect/jalaconnect/JalaconnectApplication.java
@@ -0,0 +1,13 @@
+package com.connect.jajaconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JajaconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JajaconnectApplication.class, args);
+	}
+
+}
---
+++ jalaconnect/src/test/java/com/connect/jajaconnect/JajaconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.jajaconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JajaconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
